83770bae80ccca92a764bf718bd123ef0cc7e665
jamesahn0720 <76964759+jamesahn0720@users.noreply.github.com>
2021-03-15 22:42:19 -0700

    Update Pig.java

--- Pig.java
+++ Pig.java
@@ -1,1 +1,18 @@
-//create a Pig class
+public class Pig implements Animal {
+    private String myType;
+private String mySound;
+
+Pig()
+{
+myType = "pig";
+mySound = "oink";
+}
+
+public String getSound(){
+return mySound;
+}
+
+public String getType(){
+return myType;
+}
+}
