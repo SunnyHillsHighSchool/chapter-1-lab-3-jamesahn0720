d3ac4e82191ea816bdc57c3e75bda4639d3feff1
jamesahn0720 <76964759+jamesahn0720@users.noreply.github.com>
2021-05-22 23:16:57 -0700

    Update Chick.java

--- Chick.java
+++ Chick.java
@@ -2,32 +2,13 @@ public class Chick implements Animal {
     private String myType;
     private String mySound;
 
-    Chick()
+   public Chick()
     {
         myType="chick";
         mySound="cluck";
     }
 
-    Chick(String type)
-    {
-            if(type=="old")
-            {
-                myType="chick";
-                mySound="cluck";
-            }
-           
-            else if(type=="childish")
-            {
-                myType="chick";
-                mySound="cheep";
-            }
-           
-            else
-            {
-                myType="chick";
-                mySound="cheep"+", cluck";
-            }
-    }
+  
   
    public String getSound()
     {
